0d3385736af7594467099eff4b14b6aef727a067
joey00072 <00shxf@gmail.com>
2024-08-24 16:34:40 +0530

    init
    new later
    
    blog microjax
    
    blog microjax 2
    
    blog microjax 4
    
    analytics fix kind of
    
    fix
    
    oops forgot to post it
    
    fix
    
    ckpt 1
    
    ckpt 2
    
    we dont talk about it

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -113,7 +113,9 @@ export default async function Page(props: { params: Promise<{ slug: string[] }> 
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
       <Layout content={mainContent} authorDetails={authorDetails} next={next} prev={prev}>
-        <MDXLayoutRenderer code={post.body.code} components={components} toc={post.toc} />
+        <div className="text-lg">
+          <MDXLayoutRenderer code={post.body.code} components={components} toc={post.toc} />
+        </div>
       </Layout>
     </>
   )
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,28 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Baji - Programing Language',
+    description: `First Marathi programming language, which is high-level, dynamically typed, and interpreted, facilitating programming with syntax and documentation in the Marathi language`,
+    imgSrc: '/static/images/baji.png',
+    href: 'https://github.com/joey00072/Baji-Marathi-Programing-Language',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'TinyTorch',
+    description: `Torch is autograd engine for build just under 1000 lines, Its tiny but capable of building complex models like GPT.​`,
+    imgSrc: '/static/images/cgraph-05.png',
+    href: 'https://github.com/joey00072/TinyTorch',
+  },
+  {
+    title: 'Ohara',
+    description: `Ohara, is a comprehensive collection focused on the implementation of autoregressive models, including diverse models and experiments designed for easy replication and modification, and for research and development purposes.`,
+    imgSrc: '/static/images/moe.png',
+    href: 'https://github.com/joey00072/Ohara',
+  },
+  {
+    title: 'Iza',
+    description: `Iza container runtime similar to Docker, focusing on the essentials of container functionality like chroot and syscalls for process isolation​.`,
+    imgSrc: '/static/images/iza.png',
+    href: 'https://github.com/joey00072/iza',
   },
 ]
 
